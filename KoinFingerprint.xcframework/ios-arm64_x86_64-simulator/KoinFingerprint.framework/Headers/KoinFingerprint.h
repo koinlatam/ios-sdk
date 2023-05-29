d5f1b3c5abce998526b5cd7ee62cb95ccfd6c32c
Nicolas <nicolas.gutierrez@koin.com.br>
2023-05-29 11:09:32 -0300

    Add KoinFingerprint

---
+++ KoinFingerprint.xcframework/ios-arm64_x86_64-simulator/KoinFingerprint.framework/Headers/KoinFingerprint.h
@@ -0,0 +1,18 @@
+//
+//  KoinFingerprint.h
+//  KoinFingerprint
+//
+//  Created by Nicolas Gutierrez on 25/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KoinFingerprint.
+FOUNDATION_EXPORT double KoinFingerprintVersionNumber;
+
+//! Project version string for KoinFingerprint.
+FOUNDATION_EXPORT const unsigned char KoinFingerprintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoinFingerprint/PublicHeader.h>
+
+
